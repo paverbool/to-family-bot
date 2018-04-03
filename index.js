7285439341a59b9f22e2a707ef4b9c3ebc738774
Paul Khramov <paul.khramov@namecheap.com>
2018-04-03 17:52:52 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '483619803:AAFJFT1nwEos7o6cNolJGzxaziU4loKTNyE';
 const bot = new TelegramBot(token, {polling: true});
 const notes = [];
+
+
+const Promise = require('bluebird');
+Promise.config({
+    cancellation: true
+});
+
 bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
     const userId = msg.from.id;
     const text = match[1];
@@ -9,6 +16,7 @@ bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
     notes.push({'uid': userId, 'time': time, 'text': text});
     bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
 });
+
 setInterval(function () {
     for (let i = 0; i < notes.length; i++) {
         const curDate = new Date().getHours() + ':' + new Date().getMinutes();
