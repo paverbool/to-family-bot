271a51b1ba67b286c7933b3d2eb00b9a01da9437
Paul Khramov <paul.khramov@namecheap.com>
2018-04-03 17:55:46 +0300

    init

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+process.env["NTBA_FIX_319"] = 1;
+
 const TelegramBot = require('node-telegram-bot-api');
 const token = '483619803:AAFJFT1nwEos7o6cNolJGzxaziU4loKTNyE';
 const bot = new TelegramBot(token, {polling: true});
 const notes = [];
 
 
-const Promise = require('bluebird');
-Promise.config({
-    cancellation: true
-});
+// const Promise = require('bluebird');
+// Promise.config({
+//     cancellation: true
+// });
 
 bot.onText(/\/напомни (.+) в (.+)/, function (msg, match) {
     const userId = msg.from.id;
